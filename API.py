d028ca6ec2dc8bd9f439efec59a3a0720abf9e00
guest <guest@Ubunutu.myguest.virtualbox.org>
2024-02-28 23:54:11 -0500

    python code

---
+++ API.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import pwd
+import grp
+
+app = Flask(__name__)
+
+USERNAME = 'test'
+PASSWORD = 'abcAbc123'
+
+def authenicate():
+        auth = request.authorization
+        if not auth or auth.username != USERNAME or auth.password != PASSWORD:
+             return  False
+        return True
+@app.route('/api/users',methods=['GET','POST'])
+def users():
+    if not authenticate():
+        return jsonify({'error':'Unauthorized access1111'}),401
+    users_info = {}
+    for user in pwd.getpwall():
+        user_info[str(user.pw_uid)] = user.pw_name
+    return jsonify(users_info)
+@app.route('/api/groups', methods=['GET','POST'])
+def groups():
+    if not authenticate():
+        return jsonify({'Error':'Unauthorized access2222'}), 401
+    groups_info = {}
+    for group in grp.getgrall():
+        groups_info[str(group.gr_grid)] = group.gr_name
+    return jsonify(groups_info)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=3000)
